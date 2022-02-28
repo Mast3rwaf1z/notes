c4b07eedc07513d5829d55af0e08adc5a2764b50
Thomas Jensen <thomasmollerjensen@live.dk>
2022-02-28 12:34:16 +0100

    old notes

--- 4th_semester/High_performance_programming/matrix_mult.py
+++ 4th_semester/High_performance_programming/matrix_mult.py
@@ -1,25 +1,43 @@
 from random import randint
 from multiprocessing import Process, Array
+#from threading import Thread as Process
 
-size = 10
-result = Array("i", [0 for i in range(size)])
+size = 4
+result = Array("i", [0 for i in range(size)]) # for multiprocessing
+#result = range(size) # for threading
 
-def multiply_row(index:int, row:list, integer:int) -> int:
+def multiply_row(index:int, row:list, vector:int) -> int:
     global result
     final = 0
-    for entry in row:
-        final = final + (entry * integer)
+    for i in range(len(row)):
+        final = final + (row[i] * vector[i])
     result[index] = final
-    print(f'finished task {index} with array {result[:]}')
 
+def check_process_array(array:list[Process]):
+    for process in array:
+        if process.is_alive():
+            return True
+    return False
 
-def algorithm(array:list[list], vector:list):
-    for i in range(len(array)):
-        row = array[i]
-        Process(target=lambda: multiply_row(i, row, vector[i])).start()
+def algorithm(matrix:list[list], vector:list):
+    processes:list[Process] = []
+    for i in range(len(matrix)):
+        row = matrix[i]
+        processes.append(Process(target=lambda: multiply_row(i, row, vector)))
+        processes[i].start()
+    while check_process_array(processes):
+        pass
+    return True
 
-array = [[randint(0,100) for i in range(size)] for i in range(size)]
+if __name__ == "__main__":
+    matrix = [[randint(0,100) for i in range(size)] for i in range(size)]
+    vector = [randint(0, 100) for i in range(size)]
+    algorithm(matrix, vector)
 
-vector = [randint(0, 100) for i in range(size)]
-
-algorithm(array, vector)
+    print("matrix")
+    for i in range(len(matrix)):
+        row = matrix[i]
+        print(f'row {i}: {row}')
+    print()
+    print(f'vector: {vector}')
+    print(f'result{result[:]}')
