44484f41b01133e813bd020f01baea00b18422cd
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-01-04 03:51:27 +0100

    playing around with arguments in haskell

---
+++ 7th_semester/programming_paradigms/Exam_Prep/env.hs
@@ -0,0 +1,19 @@
+import System.Environment
+
+type Command = [String]
+
+getCommand :: Command -> IO String
+getCommand (command:_) = return command
+getCommand [] = return ""
+
+getArguments :: Command -> IO [String]
+getArguments (_:args) = return args
+getArguments [] = return []
+
+main :: IO ()
+main = do
+    raws <- getArgs
+    command <- getCommand raws
+    putStrLn $ "Command:\t" ++ command
+    args <- getArguments raws
+    putStrLn $ "Arguments:\t" ++ concat (head args : [" " ++ arg | arg <- tail args])
